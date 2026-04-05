24e24b4bc44eac44ac8c03757f180c0772ddab93
KrympovskaViktoriya <krympovskav@gmail.com>
2026-04-05 16:12:21 +0300

    Завдання об'єкти

---
+++ homework_part2/task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = "Іван";
+student.lastName = "Палійчук";
+student.age = 20;
+student.courses = ["Математика", "Істория", "Програмування"];
+
+console.log(student);
---
+++ homework_part2/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "Тіні забутих предків",
+    author: "Михайло Коцюбинський",
+    year: "Іван Палійчук" // За умовою задачі тут ім'я власника
+};
+
+const { title, author } = book;
+
+console.log("Назва книги:", title);
+console.log("Автор книги:", author);
