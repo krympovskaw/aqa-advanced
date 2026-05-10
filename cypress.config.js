83fd3e49c5b27ca29d0d1053b0cb90a6d6cd3c0b
KrympovskaViktoriya <krympovskav@gmail.com>
2026-05-10 18:04:51 +0300

    feat: setup cypress and gitignore

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    viewportWidth: 1280,   
+    viewportHeight: 720,   
+    video: true,          
+    screenshotOnRunFailure: true, 
+    defaultCommandTimeout: 10000, 
+  },
+});
