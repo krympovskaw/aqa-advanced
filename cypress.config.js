4e04528d6296102d54169ddac401ee8b6bd9d504
KrympovskaViktoriya <krympovskav@gmail.com>
2026-05-17 20:20:53 +0300

    feat: add task4 cypress header and footer tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
-
   e2e: {
+    
+    specPattern: 'homework/**/*.js', 
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
-    viewportWidth: 1280,   
-    viewportHeight: 720,   
-    video: true,          
-    screenshotOnRunFailure: true, 
-    defaultCommandTimeout: 10000, 
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
   },
 });
