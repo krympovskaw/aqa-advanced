c1ba8b8770b2ed1c38849d75ee31e311b44513b2
KrympovskaViktoriya <krympovskav@gmail.com>
2026-06-07 17:33:50 +0300

    feat: implement native environment variables reader in configuration file

--- homework/registration.spec.js
+++ homework/registration.spec.js
@@ -7,7 +7,6 @@ class RegistrationModal {
    */
   constructor(page) {
     this.page = page;
-    
     this.modalTitle = page.locator('.modal-title');
     this.nameInput = page.locator('#signupName');
     this.lastNameInput = page.locator('#signupLastName');
@@ -31,27 +30,19 @@ class RegistrationModal {
   }
 }
 
-const URL = 'https://qauto.forstudy.space';
-
-test.describe('Регистрация нового пользователя - POM', () => {
-  let page;
+test.describe('Регистрация нового пользователя - POM & ENV', () => {
   let registrationModal;
 
-  test.beforeEach(async ({ browser }) => {
-    const context = await browser.newContext({
-      httpCredentials: { username: 'guest', password: 'welcome2qauto' },
-    });
-    page = await context.newPage();
+ 
+  test.beforeEach(async ({ page }) => {
     registrationModal = new RegistrationModal(page);
 
     
-    await page.goto(URL, { waitUntil: 'load' });
-    
+    await page.goto('/', { waitUntil: 'load' });
     
     await page.locator('button.header_signin').click();
     await page.locator('button:has-text("Registration")').click();
   });
-
 
   test('Успешная регистрация с валидными данными', async () => {
     const uniqueEmail = `aqa-user${Date.now()}@test.com`;
@@ -62,9 +53,7 @@ test.describe('Регистрация нового пользователя - PO
     await expect(registrationModal.garageHeader).toHaveText('Garage');
   });
 
-
   test('Негативный 1: Пустые обязательные поля', async () => {
-    
     await expect(registrationModal.registerButton).toBeVisible();
     await expect(registrationModal.garageHeader).not.toHaveText('Garage');
   });
@@ -72,37 +61,39 @@ test.describe('Регистрация нового пользователя - PO
   test('Негативный 2: Слишком короткое имя (1 символ)', async () => {
     await registrationModal.nameInput.fill('A');
     await registrationModal.lastNameInput.focus();
-    await expect(page.locator('text=Name has to be from 2 to 20 characters long')).toBeVisible();
+    await expect(registrationModal.page.locator('text=Name has to be from 2 to 20 characters long')).toBeVisible();
   });
 
   test('Негативный 3: Слишком длинная фамилия (21 символ)', async () => {
     await registrationModal.lastNameInput.fill('A'.repeat(21));
     await registrationModal.emailInput.focus();
-    await expect(page.locator('text=Last name has to be from 2 to 20 characters long')).toBeVisible();
+    await expect(registrationModal.page.locator('text=Last name has to be from 2 to 20 characters long')).toBeVisible();
   });
 
   test('Негативный 4: Невалидные symbols в имени (кириллица)', async () => {
     await registrationModal.nameInput.fill('Иван');
     await registrationModal.lastNameInput.focus();
-    await expect(page.locator('text=Name is invalid')).toBeVisible();
+    await expect(registrationModal.page.locator('text=Name is invalid')).toBeVisible();
   });
 
   test('Негативный 5: Некорректный формат Email', async () => {
     await registrationModal.emailInput.fill('aqa-wrongemail.com');
     await registrationModal.passwordInput.focus();
-    await expect(page.locator('text=Email is incorrect')).toBeVisible();
+    await expect(registrationModal.page.locator('text=Email is incorrect')).toBeVisible();
   });
 
   test('Негативный 6: Простой пароль (не соответствует требованиям)', async () => {
     await registrationModal.passwordInput.fill('12345');
     await registrationModal.repeatPasswordInput.focus();
-    await expect(page.locator('text=Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')).toBeVisible();
+    await expect(registrationModal.page.locator('text=Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')).toBeVisible();
   });
 
   test('Негативный 7: Пароли не совпадают', async () => {
     await registrationModal.passwordInput.fill('Password123');
     await registrationModal.repeatPasswordInput.fill('Password321');
     await registrationModal.nameInput.focus();
-    await expect(page.locator('text=Passwords do not match')).toBeVisible();
+    await expect(registrationModal.page.locator('text=Passwords do not match')).toBeVisible();
   });
 });
+
+  
